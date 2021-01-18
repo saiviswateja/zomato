05d83e4b910b0735f9de4f46e81f56c4335f2990
saiviswateja <saiviswateja.teja@gmail.com>
2021-01-18 20:05:06 +0530

    created the boilerplate code for server and client. Completed navbar and banner frontend components

---
+++ client/src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="banner_heading">
+                <span>Order Food Online</span>
+                <span>Best restaurants in Anantapur delivering to you doorstep</span>
+                <div className="banner_input">
+                    <select>
+                    </select>
+                    <div>
+                        <input>
+                        </input>
+                        <button>DETECT</button>
+                    </div>
+                    <button>Find Restaurants</button>
+                </div>
+                <button>Find Restaurants</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <nav>
+            <div className="logo">
+                <h4><b><i>zomato</i></b></h4>
+            </div>
+            <ul>
+                <li>
+                    Log In
+                </li>
+                <li>
+                    Create an account
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar;
