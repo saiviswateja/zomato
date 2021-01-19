e57a4e4a34acd2bc9d371a4125bddf85f91e2c74
saiviswateja <saiviswateja.teja@gmail.com>
2021-01-19 11:22:25 +0530

    added the footer on home screen

--- client/src/components/HomeFooter.js
+++ client/src/components/HomeFooter.js
@@ -4,14 +4,52 @@ import '../HomeFooter.css';
 function HomeFooter() {
     return (
         <div className="home_footer">
-            <div className="footer_head">
-                <img src="https://b.zmtcdn.com/images/logo/zomato_flat_bg_logo.svg"></img>
-                <select>
-                    <option>English</option>
-                </select>
-            </div>
             <div className="footer_about">
 
+            </div>
+            <div className="footer_links">
+                <div>
+                    <ul>
+                        <li>About Zomato</li>
+                        <li>About Us</li>
+                        <li>Culture</li>
+                        <li>Blog</li>
+                        <li>Careers</li>
+                        <li>Report Fraud</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>For Foodies</li>
+                        <li>Code of Conduct</li>
+                        <li>Community</li>
+                        <li>Verified Users</li>
+                        <li>Blogger Help</li>
+                        <li>Developers</li>
+                        <li>Mobile Apps</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                            <li>For Restaurants</li>
+                            <li>Cliam your listing</li>
+                            <li>Business App</li>
+                            <li>Business Owner Guidelines</li>
+                            <li>Business Blog</li>
+                            <li>Restaurants Widgets</li>
+                            <li>Products for Business</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>    
+                        <li>Advertise</li>
+                        <li>Order</li>
+                        <li>Book</li>
+                        <li>Trace</li>
+                        <li>Hyperpure</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
